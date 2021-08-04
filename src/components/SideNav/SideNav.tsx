9efb29c8b557499d7b37a525addc4b573ab535a4
ldippo <ldippo421@gmail.com>
2021-08-04 18:11:49 -0400

    dont render sidenav if only 1 section

--- src/components/SideNav/SideNav.tsx
+++ src/components/SideNav/SideNav.tsx
@@ -1,56 +1,53 @@
 import React from 'react';
 import { useMediaQuery } from '@react-hook/media-query';
 import { AnimatePresence } from 'framer-motion';
-import {
-	StyledSideNav,
-	SideNavItem,
-} from './SideNav.styles';
+import { StyledSideNav, SideNavItem } from './SideNav.styles';
 
 const SideNav = React.memo(function SideNav({
-	navItems,
-	selectedIdx,
-	contrastColors,
+  navItems,
+  selectedIdx,
+  contrastColors,
 }: {
-	selectedIdx: number;
-	navItems: { onClick(): void; text: string }[];
-	contrastColors: any [];
+  selectedIdx: number;
+  navItems: { onClick(): void; text: string }[];
+  contrastColors: any[];
 }) {
-	const isTiny = useMediaQuery('only screen and (max-width: 768px)');
-
-	return (
-		<AnimatePresence>
-			<StyledSideNav tiny={isTiny}>
-				{navItems.map(({ text, onClick }, idx) => {
-					return (
-						<SideNavItem
-							initial={{
-								x: -100,
-								opacity: 0,
-								color: 'white',
-								backgroundColor: 'rgba(0,0,0,0.5)',
-							}}
-							transition={{ delay: 0.5 + idx * 0.3 }}
-							animate={{
-								x: 0,
-								opacity: 1,
-								color: contrastColors[1],
-								background:
-									contrastColors[0] === 'black' ? 'rgba(0,0,0,0.5)' : 'rgba(255,255,255,0.5)',
-							}}
-							key={text}
-							text={text}
-							onClick={onClick}
-							selected={idx === selectedIdx}
-						>
-							<p>
-								{text}
-							</p>
-						</SideNavItem>
-					);
-				})}
-			</StyledSideNav>
-		</AnimatePresence>
-	);
+  const isTiny = useMediaQuery('only screen and (max-width: 768px)');
+  if (navItems.length <= 1) return <></>;
+  return (
+    <AnimatePresence>
+      <StyledSideNav tiny={isTiny}>
+        {navItems.map(({ text, onClick }, idx) => {
+          return (
+            <SideNavItem
+              initial={{
+                x: -100,
+                opacity: 0,
+                color: 'white',
+                backgroundColor: 'rgba(0,0,0,0.5)',
+              }}
+              transition={{ delay: 0.5 + idx * 0.3 }}
+              animate={{
+                x: 0,
+                opacity: 1,
+                color: contrastColors[1],
+                background:
+                  contrastColors[0] === 'black'
+                    ? 'rgba(0,0,0,0.5)'
+                    : 'rgba(255,255,255,0.5)',
+              }}
+              key={text}
+              text={text}
+              onClick={onClick}
+              selected={idx === selectedIdx}
+            >
+              <p>{text}</p>
+            </SideNavItem>
+          );
+        })}
+      </StyledSideNav>
+    </AnimatePresence>
+  );
 });
 
 export default SideNav;
