6d290e503208819e06865339b227c61878e520b1
ldippo <ldippo421@gmail.com>
2021-08-06 10:28:53 -0400

    remove color thief

--- theme/templates/cardPage.js
+++ theme/templates/cardPage.js
@@ -9,10 +9,10 @@ export default function CardPage(props) {
     const navData = pageData
         .sort((a, b) => ((a.order || 0) < (b.order || 0) ? -1 : 1))
         .map(({ title, path, templateKey, navTitle }) => ({
-            label: navTitle || title || '',
-            path: path || '#',
-            templateKey: templateKey || 'page',
-        }));
+        label: navTitle || title || '',
+        path: path || '#',
+        templateKey: templateKey || 'page',
+    }));
     const selectedPage = pageData.find(({ path }) => `/${path}` === props.path);
     const sectionData = selectedPage
         ? selectedPage.sections || []
