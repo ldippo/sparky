f275154eb100ad2094b88248b79aeb29a6687768
ldippo <ldippo421@gmail.com>
2021-08-18 08:24:55 -0400

    tweek ua config

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,8 @@ module.exports = (_options) => ({
 			resolve: 'gatsby-plugin-google-analytics',
 			options: {
 				trackingId: 'UA-45349701-2',
+				enableWebVitalsTracking: true,
+				anonymize: false,
 			},
 		},
 		'gatsby-plugin-sitemap',
